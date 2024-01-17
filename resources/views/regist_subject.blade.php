aec39a9ea6a210a2146968b8bd9830d8ab1a2962
shimaoka hitoshi <shimaoka1203@gmail.com>
2024-01-17 20:42:21 +0900

    初期設定(途中)

---
+++ resources/views/regist_subject.blade.php
@@ -0,0 +1,18 @@
+<?php
+
+echo $_POST['test'];
+
+// $array = [
+//     [
+//         'id' => 1,
+//         'name' => 'テスト敵1',
+//     ],
+//     [
+//         'id' => 2,
+//         'name' => 'テスト敵2',
+//     ],
+// ];
+
+// header('Access-Control-Allow-Origin: *');
+
+// echo json_encode($array);
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('sample');
+});
+
+Route::post('/regist', function () {
+    return view('regist_subject');
 });
